8a306014e4e128764495acea18ba9be9e0ebaa79
raghavkukkar <raghavkukkar9@gmail.com>
2020-08-02 15:02:50 +0530

    pomodoro logic added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Timer from './components/Timer/Timer'
 import Buttons from './components/Buttons/Buttons'
 import Modal from './components/Modal/Modal'
+import Nav from './components/Nav/Nav'
 let s = null
 class App extends Component {
   constructor(props) {
@@ -11,60 +12,92 @@ class App extends Component {
       time: 10,
       timer : 10,
       file: "/Assets/beat.mp3",
-      modalIsOpen : false
-    }
+      modalIsOpen: false,
+      break: false,
+      breakCount: 1    }
   }
+
+  //modal submit handler
   timeChanger = (x,e) => {  
     this.setState({
       time: x,
-      timer : x
+      timer: x
     })
     e.preventDefault();
     this.openModal();
   }
+
+  //modal opener and closer
   openModal = () => {
     this.setState({
       modalIsOpen : !this.state.modalIsOpen
     })
   }
-
+  // audio play
   playAudio = (file) =>{
     let audio = new Audio(file);
     audio.play();
   }
 
-  startTimer = () => {
-    if (!this.state.time) alert("reset it");
-    else {
-      s = setInterval(() => {
-        this.setState({
-          timer: this.state.timer - 1
-        })
-        if (!this.state.timer) {
-          this.stopTimer();
-          this.playAudio(this.state.file);
-        }
-      }, 1000)
-    
-    }
-  }
-
+//stopper function
   stopTimer = () => {
     clearInterval(s)
   }
 
-
+  //timer reseter
   resetTimer = () => {
     clearInterval(s)
     this.setState({
       timer : this.state.time
     })
   }
+  //break manager
+  break() {
+   let  x = !(this.state.breakCount % 4) ? 11 : 3;
+    this.setState({
+      time: x,
+      timer: x,
+      breakCount: this.state.breakCount + 1,
+      break: !this.state.break
+    });
+    this.startTimer();
+  }
 
+  //manage timer
+startTimer = () => {
+  if (!this.state.time) alert("reset it");
+  else {
+    s = setInterval(() => {
+      this.setState({
+        timer: this.state.timer - 1
+      })
+      if (!this.state.timer) {
+        this.stopTimer();
+        if (!this.state.break) {
+          if (window.confirm("start the break")) {
+            this.break();
+          }
+        }
+        else {
+          if (window.confirm("reset the pomodoro")) {
+            this.setState({
+              time: 15,
+              timer: 15,
+              break : !this.state.break
+            })
+          }
+        }
+       
+      }
+    }, 1000)
+  
+  }
+}
   render() {
     return (
       <React.Fragment>
-      <div className = {this.state.modalIsOpen? "overlay":""}></div>  
+        <div className={this.state.modalIsOpen ? "overlay" : ""}></div> 
+      <Nav/>  
       <div className="timer-container">
         <Timer time={`${Math.floor(this.state.timer / 60)}:${parseInt(this.state.timer % 60)}`} />
         <Buttons startTimer={this.startTimer} stopTimer={this.stopTimer} resetTimer={this.resetTimer} changeTime={this.openModal} />
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Nav extends Component{
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div className="nav">
+                <span> Timers</span>
+            </div>
+        );
+    }
+}
+
+export default Nav
